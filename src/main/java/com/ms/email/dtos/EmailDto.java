a28158e1d4f514e4c81921ee6a69a3a0494de854
Gustavo-GPG <gustavogpg23@gmail.com>
2024-09-23 14:34:08 -0300

    Criando dto para email

--- src/main/java/com/ms/email/dtos/EmailDto.java
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -1,5 +1,21 @@
 package com.ms.email.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class EmailDto {
 
+  @NotBlank
+  private String propietarioDoEmail;
+  @NotBlank
+  private String remetente;
+  @NotBlank
+  @Email
+  private String destinatario;
+  @NotBlank
+  private String titulo;
+  @NotBlank
+  private String texto;
 }
