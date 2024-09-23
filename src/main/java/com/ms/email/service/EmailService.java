a9c45da110d31f905042f0282cc56fe8ce280c5e
Gustavo-GPG <gustavogpg23@gmail.com>
2024-09-23 15:12:56 -0300

    Implementando envio de email

--- src/main/java/com/ms/email/service/EmailService.java
+++ src/main/java/com/ms/email/service/EmailService.java
@@ -1,8 +1,12 @@
 package com.ms.email.service;
 
+import com.ms.email.enums.EmailStatus;
 import com.ms.email.model.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +14,29 @@ public class EmailService {
   @Autowired
    private final EmailRepository emailRepository;
 
+  @Autowired
+  private JavaMailSender emailSender;
+
   public EmailService(EmailRepository emailRepository) {
     this.emailRepository = emailRepository;
   }
 
   public void enviarEmail(EmailModel emailModel) {
-    emailRepository.save(emailModel);
+
+    try {
+      SimpleMailMessage menssagem = new SimpleMailMessage();
+      menssagem.setFrom(emailModel.getRemetente());
+      menssagem.setTo(emailModel.getDestinatario());
+      menssagem.setSubject(emailModel.getTitulo());
+      menssagem.setText(emailModel.getTexto());
+      emailSender.send(menssagem);
+
+      emailModel.setStatusDoEmail(EmailStatus.ENVIADO);
+    } catch (MailException e) {
+      emailModel.setStatusDoEmail(EmailStatus.ERROR);
+    } finally {
+      emailRepository.save(emailModel);
+    }
 
   }
 }
