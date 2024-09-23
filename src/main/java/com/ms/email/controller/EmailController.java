6738e8fd3c7ef7b0c1625d4007cf35e9069ea86f
Gustavo-GPG <gustavogpg23@gmail.com>
2024-09-23 14:50:14 -0300

    Implementando método de enviar email em controller

--- src/main/java/com/ms/email/controller/EmailController.java
+++ src/main/java/com/ms/email/controller/EmailController.java
@@ -1,7 +1,13 @@
 package com.ms.email.controller;
 
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.model.EmailModel;
 import com.ms.email.service.EmailService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +17,13 @@ public class EmailController {
 
   public EmailController(EmailService emailService) {
     this.emailService = emailService;
+  }
+
+  @PostMapping("/sending-email")
+  public ResponseEntity<EmailModel> enviarEmail(EmailDto emailDto) {
+    EmailModel emailModel= new EmailModel();
+    BeanUtils.copyProperties(emailDto, emailModel);
+    emailService.enviarEmail(emailModel);
+    return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
   }
 }
