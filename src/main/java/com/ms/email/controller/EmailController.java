d24c93bcfbcc3c63e3f0db7c4be7cd58fbb7c676
Gustavo-GPG <gustavogpg23@gmail.com>
2024-09-23 14:55:01 -0300

    Iniciando implementação do service

--- src/main/java/com/ms/email/controller/EmailController.java
+++ src/main/java/com/ms/email/controller/EmailController.java
@@ -3,11 +3,13 @@ package com.ms.email.controller;
 import com.ms.email.dtos.EmailDto;
 import com.ms.email.model.EmailModel;
 import com.ms.email.service.EmailService;
+import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +22,7 @@ public class EmailController {
   }
 
   @PostMapping("/sending-email")
-  public ResponseEntity<EmailModel> enviarEmail(EmailDto emailDto) {
+  public ResponseEntity<EmailModel> enviarEmail(@RequestBody @Valid EmailDto emailDto) {
     EmailModel emailModel= new EmailModel();
     BeanUtils.copyProperties(emailDto, emailModel);
     emailService.enviarEmail(emailModel);
--- src/main/java/com/ms/email/service/EmailService.java
+++ src/main/java/com/ms/email/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.ms.email.service;
 
+import com.ms.email.model.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,10 @@ public class EmailService {
 
   public EmailService(EmailRepository emailRepository) {
     this.emailRepository = emailRepository;
+  }
+
+  public void enviarEmail(EmailModel emailModel) {
+    emailRepository.save(emailModel);
+
   }
 }
